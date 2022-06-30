3762d94dc7b7dd84b8ed20efafc399b383b36089
Henry Thomas <sigma1198@gmail.com>
2022-06-30 16:24:20 -0700

    intro

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Good evening 🧛
-Welcome to my Python curriculum. In this course you will learn the forbidden jutsu that is Python programming. 
+Welcome to my Python curriculum. In this course you will learn the forbidden jutsu that is Python programming.
+
+Additionally we will be learning how to use Github as we go along.
 
 # Setup
 We will be installing python 3.10.5
@@ -10,4 +12,19 @@ We will be installing python 3.10.5
 ## Mac
 [Install Here](https://www.python.org/ftp/python/3.10.5/python-3.10.5-macos11.pkg)
 
+# Lesson Order
+The curriculum is designed to be consumed in the following order:
+|  No.  | Section     |
+| ----- | ----------- |
+|   1   | intro       |
+|   2   | variables   |
+|   3   | conditionals|
+|   4   | loops       |
+|   5   | functions   |
+|   6   | classes     |
+|   7   | modules     |
+|   8   | exceptions  |
+|   9   | input_and_output|
+|   10  | flask       |
+|   11  | virtual_envs|
 
---
+++ intro/intro.py
@@ -0,0 +1,44 @@
+# Welcome to Python!
+# This is how you write a single line comment (start the line with a hashtag)
+
+# Comments are useful for documenting code (i have shit memory, so I use comments often to remember wtf I was doing)
+
+"""
+You can also write multi line comment as well! Simply
+open three quotation marks and bam
+
+you're writing a multi line comment
+"""
+
+# There are two main ways that you can run python code
+# 1. Directly writing python in the interpreter ex:
+#   C:\Users\Hanky>python
+#   Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+#   Type "help", "copyright", "credits" or "license" for more information.
+#   >>> print("Hello World")
+#   "Hello World"
+
+# 2. Telling the interpreter which file to run ex:
+#   C:\Users\Hanky>python my_script.py
+#   Hello World
+
+# When should I type my code directly in the interpreter vs saving it to a file?
+#   If you are only running a small amount of code (I would say 5 lines maximum) then you can run your code directly in the interpreter.
+#   When your code starts to get long, it can be difficult to manage only using the interpreter, thus it makes sense to save your code to a file, and then run that file.
+
+
+# Our first program..... "Hello World"
+#   Since this is only one line of code in this program, let's run this directly in the interpreter
+#   1. From your command line (CMD on Windows, Terminal on Mac) type "python" ex:
+#       C:\Users\Hanky>python
+#   2. This should open the python interpreter, it will look like this:
+#       Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+#       Type "help", "copyright", "credits" or "license" for more information.
+#       >>>
+#   3. type print("hello world") and hit enter. If everything worked, you should see "hello world" under the line you just typed
+
+# you can use the print function to print out any string you want to the terminal
+# this is very helpful for showing you what your program is doing as it is executing
+
+
+print("hello world")
