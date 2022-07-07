e1267cbd121ed7f0a8054d5571ccca842e0044ee
Henry Thomas <sigma1198@gmail.com>
2022-07-06 18:29:55 -0700

    Revert "Update strings.py"
    This reverts commit 0f193b753ac4d18947270b111e43cdb9c4934b7c.

--- variables/strings.py
+++ variables/strings.py
@@ -49,37 +49,7 @@ anotherString = "world"
 print("hello {var}".format(var=anotherString))
 
 # this is very useful when you need to combine several variables together to make one string
-# let's try combining several variables together to make one string
 
-var1 = "get"
-var2 = 0
-var3 = "ches"
 
-print("You {x} {y} bit{z}".format(x=var1, y=var2, z=var3))
 
-# what did it print out? :-)
 
-# another cool feature of strings is that you can index them
-# indexing allows you to say "give me the thing at this position"
-# ex
-myString = "python"
-
-# this is saying print the character at index 2
-print(myString[2]) # this prints "t"... but why "t" instead of "y"?
-
-# in python, indexes start at 0, so if we look at the string above
-# Index:  0 1 2 3 4 5
-# String: p y t h o n
-
-# we are saying get us the letter at index 2... which gives us "t"
-
-# the last thing I want to show here for strings, is that you can get their length
-# ex
-newString = "good evening"
-print(len(newString)) # this gives us 12
-
-# HOMEWORK
-# Create a new variable, set it equal to the state (or providence) that you live in
-# Create a second variable, set it equal to the length of your first variable
-# Print out the following message "The state I live in is <YOUR STATE> and it is <STATE LENGTH> characters long"
-# You should use the .format() method to create the message I want you to print
